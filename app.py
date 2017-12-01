d22cc0385d26effa66a56a2130ca51114e59f918
Abhishek <abhisheksubu92@gmail.com>
2017-12-01 15:41:05 +0530

    origin

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def prediction():
 	loaded_model = None
 	with open(clf,'rb') as f:
 		loaded_model = pickle.load(f)
-	predictions = loaded_model.predict(value.values.reshape(1,-1))
+	predictions = loaded_model.predict(value)
 	# prediction_series = list(pd.Series(predictions))
 	# final_predictions = pd.DataFrame(list(zip(loan_ids, prediction_series)))
 	# responses = jsonify(predictions=final_predictions.to_json(orient="records"))
